50d353b5e6e562bcc3eef3d145fc4e803d6d676f
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-11-11 17:40:44 -0600

    update editorconfig

--- cluckcheck.scm
+++ cluckcheck.scm
@@ -1,28 +1,28 @@
 (module cluckcheck (gen-int gen-bool gen-char gen-list gen-string for-all)
-	(import scheme chicken extras)
+  (import scheme chicken extras)
 
-	(use srfi-1) ; lists
+  (use srfi-1) ; lists
 
-	(define (gen-int)
-		(random 256))
+  (define (gen-int)
+    (random 256))
 
-	(define (gen-bool)
-		(= (random 2) 0))
+  (define (gen-bool)
+    (= (random 2) 0))
 
-	(define (gen-char)
-		(integer->char (random 128)))
+  (define (gen-char)
+    (integer->char (random 128)))
 
-	(define (gen-list gen)
-		(map (lambda (x) (gen)) (iota (random 100))))
+  (define (gen-list gen)
+    (map (lambda (x) (gen)) (iota (random 100))))
 
-	(define (gen-string)
-		(list->string (gen-list gen-char)))
+  (define (gen-string)
+    (list->string (gen-list gen-char)))
 
-	(define (for-all property . generators)
-		(let ((values (map (lambda (x) (map (lambda (f) (f)) generators)) (iota 100))))
-			(let ((failure (find (lambda (vs) (not (apply property vs))) values)))
-				(let ((failed (list? failure)))
-					(if failed
-							(display (format "*** Failed!\n~a\n" failure))
-							(display (format "+++ OK, passed 100 tests\n")))
-					failed)))))
+  (define (for-all property . generators)
+    (let ((values (map (lambda (x) (map (lambda (f) (f)) generators)) (iota 100))))
+      (let ((failure (find (lambda (vs) (not (apply property vs))) values)))
+        (let ((failed (list? failure)))
+          (if failed
+              (display (format "*** Failed!\n~a\n" failure))
+              (display (format "+++ OK, passed 100 tests\n")))
+          failed)))))
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,16 +1,16 @@
 Given(/^the program has finished$/) do
-  @cucumber = `csi example.scm`
+    @cucumber = `csi example.scm`
 end
 
 Then(/^the output is correct for each test$/) do
-  lines = @cucumber.split("\n")
+    lines = @cucumber.split("\n")
 
-  lines.length.should == 18
+    lines.length.should == 18
 
-  lines[0 .. 13].join("\n").should =~ /^.*(; loading .*)+/m
+    lines[0 .. 13].join("\n").should =~ /^.*(; loading .*)+/m
 
-  lines[14].should == '*** Failed!'
-  lines[15].should =~ /^\([0-9]+\)$/
-  lines[16].should == '+++ OK, passed 100 tests'
-  lines[17].should == '+++ OK, passed 100 tests'
+    lines[14].should == '*** Failed!'
+    lines[15].should =~ /^\([0-9]+\)$/
+    lines[16].should == '+++ OK, passed 100 tests'
+    lines[17].should == '+++ OK, passed 100 tests'
 end
